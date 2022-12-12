d8547bbb1c816ac02264cb9fa00ae8d79f3b0720
beknurkyyrgyzbaev <arapovbeknur@gmail.com>
2022-12-12 20:08:46 +0600

    agahjyh

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println();
         System.out.println();
         System.out.println();
-        System.out.println();
+
+        System.out.println(); System.out.println(); System.out.println();
     }
 }
